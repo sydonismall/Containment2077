5a89aeb1f6261a80260417aabd7a842c8e5f683f
jjessica-ho <jessica.nh7890@gmail.com>
2025-04-01 08:56:03 -0500

    cododee

---
+++ Containment 2077/Assets/Scripts/Main Game.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+public class MainGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        System.Random rnd = new System.Random();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
